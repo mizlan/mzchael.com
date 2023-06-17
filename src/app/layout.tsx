1c2c284a290da0401c288611a23035bbb7bf123d
Michael Lan <michaellan202@gmail.com>
2023-06-17 15:07:55 -0700

    fix: edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Providers from './Providers'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -16,13 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={"${inter.className} min-h-screen flex flex-col"}>
         <nav>
           <ul>
-            <li><a href="https://github.com/mizlan">github</a>  </li>
-            <li><a href="https://www.youtube.com/c/mzzzchael">youtube</a> </li>
+            <li><a href="https://github.com/mizlan">github</a></li>
+            <li><a href="https://www.youtube.com/c/mzzzchael">youtube</a></li>
+            <li><a href="https://www.instagram.com/mzzzchael/">portfolio</a></li>
+            <li><a href="https://www.instagram.com/mzzzchael/">this site</a></li>
             <li><a href="https://blog.mzchael.com">blog</a> </li>
-            <li><a href="https://www.instagram.com/mzzzchael/">instagram</a></li>
+            {/* <li><a href="https://www.instagram.com/mzzzchael/">instagram</a></li> */}
           </ul>
         </nav>
         <Providers>
