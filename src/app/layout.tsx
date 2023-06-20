e57d3f9ff08308923917e7ec7c0184bd343c3a43
Michael Lan <michaellan202@gmail.com>
2023-06-20 10:11:48 -0700

    feat: add nav links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import Providers from './Providers'
 import ThemeToggle from './ThemeToggle'
+import NavLink from './NavLink'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,19 +18,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={"${inter.className} min-h-screen flex flex-col"}>
-        <nav>
-          <ul>
-            <li><Link href="/">home</Link></li>
-            <li><a href="https://github.com/mizlan">github</a></li>
-            <li><a href="https://www.youtube.com/c/mzzzchael">youtube</a></li>
-            <li><Link href="/this-site">this site</Link></li>
-            <li><a href="https://blog.mzchael.com">blog</a> </li>
-          </ul>
-        </nav>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <Providers>
+          <nav>
+            <ul>
+              <li><NavLink href="/">home</NavLink></li>
+              <li><NavLink href="https://github.com/mizlan">github</NavLink></li>
+              <li><NavLink href="https://www.youtube.com/c/mzzzchael">youtube</NavLink></li>
+              <li><NavLink href="/this-site">this site</NavLink></li>
+              <li><NavLink href="https://blog.mzchael.com">blog</NavLink> </li>
               <ThemeToggle />
+            </ul>
+          </nav>
+          {children}
+        </Providers>
       </body>
     </html>
   )
