7310848694604a8554506fb4499879d5e27d79ec
Michael Lan <michaellan202@gmail.com>
2023-06-21 12:22:15 -0700

    refactor: Refactor ThemeToggle

--- src/app/ThemeToggle.tsx
+++ src/app/ThemeToggle.tsx
@@ -6,6 +6,22 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 
+const Sun = () => (
+  <Image
+    src={sun}
+    alt=""
+    className="w-3"
+  />
+)
+
+const Moon = () => (
+  <Image
+    src={moon}
+    alt=""
+    className="w-3"
+  />
+)
+
 const ThemeToggle = () => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
@@ -20,29 +36,10 @@ const ThemeToggle = () => {
   }
 
   return (
-    <div className="relative inline-block w-8 h-4 cursor-pointer rounded-full self-center">
+    <div className="relative inline-block w-8 h-4 cursor-pointer rounded-full">
       <input id="switch-component" type="checkbox" checked={theme === 'dark'} disabled={!mounted} className="peer appearance-none w-8 h-4 absolute bg-rosePearl-800/20 rounded-full cursor-pointer transition-colors duration-300 checked:bg-lavenderHaze-300/30" onChange={callback} />
       <label htmlFor="switch-component" className="bg-rosePearl-100 w-5 h-5 border border-[#e3dfda] rounded-full shadow-md absolute top-2/4 -left-1 -translate-y-2/4 peer-checked:translate-x-full transition-all duration-300 cursor-pointer peer-checked:bg-theme-dark peer-checked:border-[#2e232e] flex justify-center items-center">
-        {
-          !mounted ?
-            <Image
-              src={sun}
-              alt=""
-              className="w-3"
-            /> :
-            theme !== 'dark' ?
-              <Image
-                src={sun}
-                alt=""
-                className="w-3"
-              />
-              :
-              <Image
-                src={moon}
-                alt=""
-                className="w-3"
-              />
-        }
+        {(!mounted || theme !== 'dark') ? <Sun /> : <Moon />}
       </label>
     </div>
   )
