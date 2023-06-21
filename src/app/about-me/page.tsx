ed3cd0fe8d40e3641df18df69577d82c7b32b845
Michael Lan <michaellan202@gmail.com>
2023-06-21 16:20:41 -0700

    feat: about me

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,49 @@
+import '../globals.css'
+
+const AboutMe = () => {
+  return (
+    <main className="flex flex-col gap-4 px-10 sm:px-[15vw] lg:px-[30vw] mb-10">
+      <section className="flex flex-col gap-3">
+        <h1 className="text-3xl font-extrabold">More About Me</h1>
+        <p>
+          I&apos;m a second-year at UCLA majoring in computer science and minoring in philosophy.
+          My research interests include functional programming, programming language design,
+          and compilers.
+        </p>
+        <p>
+          I also love software engineering&#8212;I have 2+ years of experience in full-stack software
+          development and DevOps using modern technologies
+          (primarily in Python, Node, TypeScript, and React).
+        </p>
+      </section>
+      <section className="flex flex-col gap-3">
+        <h2 className="text-2xl font-extrabold">Open Source</h2>
+        <p>
+          I am a big advocate of open-source software and the free software philosophy.
+          To that end, I am an avid contributor to open-source projects I use
+          (see my GitHub for contribution activity), and I am also
+          an open-source maintainer, with my most notable open-source project being
+          {' '}
+          <a href="https://github.com/mizlan/iswap.nvim">iswap.nvim</a>,
+          an interactive refactoring tool for the Neovim text editor.
+        </p>
+        <p>
+          I also maintain the UCLA ACM TeachLA Editor, an open-source, full-stack
+          kid-friendly text editor used by ACM TeachLA to teach kids how to program!
+        </p>
+        <p>
+          Yeah. I&apos;m just really into text editors, it seems :)
+        </p>
+      </section>
+      <section className="flex flex-col gap-3">
+        <h2 className="text-2xl font-extrabold">Reach Out!</h2>
+        <p>
+          I&apos;d love to talk with you about anything! Reach me at michaellan AT ucla DOT edu.
+          It might take a few days for me to respond.
+        </p> 
+      </section>
+    </main>
+  )
+}
+
+export default AboutMe
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
           <nav className="flex flex-row justify-between items-start gap-12 mt-5 mb-10 md:items-center lg:justify-end px-10 md:px-20 lg:px-32">
             <ul className="flex flex-col justify-between md:items-end md:flex-row md:justify-end md:gap-7">
               <li className="mb-1.5 md:mb-0"><NavLink href="/">home</NavLink></li>
+              <li className="mb-1.5 md:mb-0"><NavLink href="/about-me">about me</NavLink></li>
               <li className="mb-1.5 md:mb-0"><NavLink href="https://github.com/mizlan">github</NavLink></li>
               <li className="mb-1.5 md:mb-0"><NavLink href="https://www.youtube.com/c/mzzzchael">youtube</NavLink></li>
               <li className="mb-1.5 md:mb-0"><NavLink href="/this-site">this site</NavLink></li>
