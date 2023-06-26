727797d06a41cf38e5b43724393552eb94bfe2ea
Michael Lan <michaellan202@gmail.com>
2023-06-26 12:14:03 -0700

    feat: add MDX support for blogs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: "@mdx-js/react",
+  },
+})
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
+}
+ 
+module.exports = withMDX(nextConfig)
