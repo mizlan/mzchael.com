97027369115adf496d27c619795c8af17ae7af3b
Michael Lan <michaellan202@gmail.com>
2023-07-04 08:37:01 -0700

    feat: add separate navigation

--- src/app/NavLink.tsx
+++ src/app/NavLink.tsx
@@ -4,8 +4,9 @@ import Image from "next/image"
 import Link from "next/link"
 import arrow from "public/arrow.svg"
 import { useSelectedLayoutSegment } from 'next/navigation'
+import * as NavigationMenu from '@radix-ui/react-navigation-menu'
 
-const NavLink = ({
+const NavItem = ({
   href,
   children,
 }: {
@@ -19,18 +20,22 @@ const NavLink = ({
 
   if (!href.startsWith("/")) {
     return (
-      <div className="flex flex-row items-center gap-1">
-        <Link target="_blank" className='text-rosePearl-700 dark:text-rosePearl-600' href={href}>{children}</Link>
+      <NavigationMenu.Item className="flex flex-row items-center gap-1 mb-1.5 md:mb-0">
+        <NavigationMenu.Link asChild>
+          <Link target="_blank" className='text-rosePearl-700 dark:text-rosePearl-600' href={href}>{children}</Link>
+        </NavigationMenu.Link>
         <Image src={arrow} alt="" className="w-1.5 h-1.5 -translate-y-1" />
-      </div >
+      </NavigationMenu.Item>
     )
   }
 
   return (
-    <div className="flex flex-col justify-center">
-      <Link className={isActive ? `underline text-rosePearl-900 dark:text-rosePearl-300` : `text-rosePearl-700 dark:text-rosePearl-600`} href={href}>{children}</Link>
-    </div>
+    <NavigationMenu.Item className="flex flex-row items-center gap-1 mb-1.5 md:mb-0">
+      <NavigationMenu.Link asChild>
+        <Link className={isActive ? `underline underline-offset-4 decoration-2 text-rosePearl-900 dark:text-rosePearl-300` : `text-rosePearl-700 dark:text-rosePearl-600`} href={href}>{children}</Link>
+      </NavigationMenu.Link>
+    </NavigationMenu.Item>
   )
 }
 
-export default NavLink
+export default NavItem
