753503aa17a109410649fa5d3a2a3350e397cc46
Michael Lan <michaellan202@gmail.com>
2024-03-15 14:54:11 -0700

    feat: Animate underlines in navigation links

--- src/app/NavLink.tsx
+++ src/app/NavLink.tsx
@@ -18,14 +18,14 @@ const NavItem = ({
   return (
     <NavigationMenu.Item className="mb-1.5 flex flex-row items-center gap-1 md:mb-0">
       <NavigationMenu.Link
-        className="focus:text-rosePearl-900 dark:focus:text-rosePearl-300"
+        className="after:block after:h-0.5 focus:text-rosePearl-900 dark:focus:text-rosePearl-300"
         asChild
       >
         <Link
           className={
             isActive
-              ? `text-rosePearl-900 underline decoration-2 underline-offset-4 dark:text-rosePearl-300`
-              : `text-rosePearl-700 dark:text-rosePearl-600`
+              ? `text-rosePearl-900 after:w-full after:bg-rosePearl-900 dark:text-rosePearl-300 dark:after:bg-rosePearl-300`
+              : `text-rosePearl-700 after:w-0 after:bg-rosePearl-700 after:transition-[width] after:duration-500 hover:after:w-full focus:after:bg-rosePearl-900 dark:text-rosePearl-600 dark:after:bg-rosePearl-600 dark:focus:after:bg-rosePearl-300`
           }
           href={href}
         >
