4b7ce895689a244359c7e7d8acac1b7210c99142
Michael Lan <michaellan202@gmail.com>
2024-10-15 10:19:07 -0700

    don't display ThemeToggle on phone

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -3,11 +3,13 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import ThemeToggle from "./ThemeToggle";
 import NavItem from "./NavLink";
+import { useMediaQuery } from "usehooks-ts";
 
 const Navigation = () => {
+  const matches = useMediaQuery("(min-width: 640px)");
   return (
     <NavigationMenu.Root
-      className={`text-sm md:text-base mb-3 md:mb-10 mt-5 flex flex-row items-start justify-between gap-12 px-10 font-generalsans font-medium uppercase tracking-wide md:items-center md:px-20 lg:px-32 xl:justify-end`}
+      className={`mb-3 mt-5 flex flex-row items-start justify-between gap-12 px-10 font-generalsans text-sm font-medium uppercase tracking-wide md:mb-10 md:items-center md:px-20 md:text-base lg:px-32 xl:justify-end`}
     >
       <NavigationMenu.List className="flex flex-row items-end justify-end gap-7">
         <NavItem href="/">home</NavItem>
@@ -15,7 +17,8 @@ const Navigation = () => {
         <NavItem href="/site">site</NavItem>
         <NavItem href="/blog">blog</NavItem>
       </NavigationMenu.List>
-      <ThemeToggle />
+
+      {matches && <ThemeToggle />}
     </NavigationMenu.Root>
   );
 };
