b2011e09cf189b67ce1705d58a3e6d5ce3132b2e
Michael Lan <michaellan202@gmail.com>
2025-04-05 21:30:03 -0700

    upd

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,21 +12,10 @@ const About = () => {
           <Link href="https://boardgamegeek.com/collection/user/BallOfSalt">
             board game collection
           </Link>
-          .
-        </p>
-      </section>
-      <section className="space-y-3">
-        <Heading>Open Source</Heading>
-        <p>
-          I am an avid open-source contributor and maintainer, with my most
-          notable open-source project being{" "}
-          <Link href="https://github.com/mizlan/iswap.nvim">iswap.nvim</Link>,
-          an interactive refactoring tool for the Neovim text editor.
-        </p>
-        <p>
-          In my second year at UCLA, I was also the maintainer of the UCLA ACM
-          TeachLA Editor, an open-source text editor used by ACM TeachLA to
-          teach kids how to program!
+          . Some technologies and tools I use and love are: Kitty, a terminal
+          emulator; Neovim, a text editor; Kagi, a search engine; and Typst, a
+          typesetting tool. I would also like to use a Framework laptop, but my
+          current laptop works just fine for now.
         </p>
       </section>
       <section className="space-y-3">
