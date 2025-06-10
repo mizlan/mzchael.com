234f643fd73cf89bee066ff66d0b6efc29d3a199
Michael Lan <michaellan202@gmail.com>
2025-06-09 22:53:59 -0700

    Some text updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,17 +12,21 @@ const About = () => {
           <Link href="https://boardgamegeek.com/collection/user/BallOfSalt">
             board game collection
           </Link>
-          . Some technologies and tools I use and love are: Kitty, a terminal
-          emulator; Neovim, a text editor; Kagi, a search engine; and Typst, a
-          typesetting tool. I would also like to use a Framework laptop, but my
-          current laptop works just fine for now.
+          . Some technologies and tools I use and love are: Emacs, an extensible
+          Lisp system, Kitty, a terminal emulator; Neovim, a text editor; Kagi,
+          a search engine; and Typst, a typesetting tool. I would also like to
+          use a Framework laptop, but my current laptop works just fine for now.
+          When I have time, I{" "}
+          <Link href="https://www.youtube.com/c/mzzzchael">make videos</Link>{" "}
+          and <Link href="https://github.com/mizlan">write code</Link>.
+          Ostensibly, I don&apos;t have much time.
         </p>
       </section>
       <section className="space-y-3">
         <Heading>Reach Out!</Heading>
         <p>
           I&apos;d love to talk with you about anything! Reach me at michaellan
-          AT ucla DOT edu. It might take a few days for me to respond.
+          AT ucla DOT edu.
         </p>
       </section>
     </main>
