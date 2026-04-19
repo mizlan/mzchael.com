9a5ec7bff094ef8a8e9a1ca23ea45c57aea39d1f
Michael Lan <michaellan202@gmail.com>
2026-04-18 18:23:48 -0700

    shorten

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,23 @@ const Home = async () => {
         <Heading className="text-xl">Michael Lan</Heading>
         <p className="text-md">
           I am a student at UCLA, where I am pursuing my bachelor&apos;s in
-          computer science along with a minor in philosophy. Although I&apos;m
-          not actively conducting research at present, I enjoy thinking about
-          functional programming and formal methods. In the past, I&apos;ve
-          contributed to mechanized Rocq proofs of certain{" "}
+          computer science along with a minor in philosophy. I enjoy thinking
+          about functional programming and formal methods. My academic
+          background involves coursework in compilers, programming language
+          theory, and type theory. In the past, I&apos;ve contributed to
+          mechanized Rocq proofs of certain{" "}
           <Link href="https://doi.org/10.1016/j.laa.2024.03.039">
             quantum computing results
           </Link>
-          . My academic background involves coursework in compilers, programming
-          language theory, and type theory. Outside of these academic pursuits,
-          I also enjoy building open source developer tools.
+          . I also enjoy building open source developer tools.
         </p>
         <p>
-          Moving forward, however, my career path is largely oriented toward
-          industry, so to speak. I&apos;ve nonetheless always tried to focus my
-          attention on using functional programming languages. Previously, I
-          interned at Pure&nbsp;Storage (now known as Everpure). More recently,
-          I interned at Tesla, where I wrote Haskell; and at Jane Street, where
-          I wrote OCaml. After graduation I will be joining Jane&nbsp;Street
-          full-time as a software engineer.
+          My future, however, is oriented toward industry, but involving
+          functional programming where possible. I've interned at
+          Pure&nbsp;Storage (now known as Everpure). More recently, I interned
+          at Tesla, where I wrote Haskell; and at Jane Street, where I wrote
+          OCaml. After graduation I will be joining Jane&nbsp;Street full-time
+          as a software engineer.
         </p>
         <p>
           My hobbies include philosophy, karaoke, poker, guitar, and crosswords.
@@ -37,15 +35,12 @@ const Home = async () => {
           <Link href="https://boardgamegeek.com/collection/user/BallOfSalt">
             board game collection
           </Link>{" "}
-          which rarely sees the light of day. This is a tragic but widespread
-          and well-documented phenomenon&mdash;at least I can say none of my
-          games are still in the original shrinkwrap. I&apos;d rather be a board
-          game player than a board game collector. I also care a lot about the
+          which rarely sees the light of day. I also care a lot about the
           particular tools I use. Though I used Neovim for a number of years, I
           have since switched to Emacs. I also use and love: Kitty, a terminal
-          emulator; Kagi, a search engine; and Typst, a typesetting engine. Try
-          them out! On another note, I have also taken it upon myself to read
-          more fiction in the coming year. These days, I occasionally{" "}
+          emulator; Kagi, a search engine; Typst, a typesetting engine; and
+          Jujutsu, a version control frontend. Try them out! These days, I
+          occasionally {/* FIXME: add links */}
           <Link href="https://github.com/mizlan">write code</Link> and even less
           frequently{" "}
           <Link href="https://www.youtube.com/c/mzzzchael">make videos</Link>.
